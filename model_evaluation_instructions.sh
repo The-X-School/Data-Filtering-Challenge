ecdd9d0ca52ec0490f90a344b7963c68594f7767
Lindsey Li <lli065@student.fuhsd.org>
2025-06-13 09:15:48 -0700

    evaluation code

--- model_evaluation_instructions.sh
+++ model_evaluation_instructions.sh
@@ -1,43 +1,37 @@
-## Model evaluation instructions 
-I changed a few lines of code, it worked when I ran it on command line on Mac
-```
+# Model evaluation instructions 
+#I changed a few lines of code, it worked when I ran it on command line on Mac
+
 git clone -b data4elm https://github.com/OptimalScale/LMFlow.git
 cd LMFlow
 conda create -n lmflow python=3.9 -y
 conda activate lmflow
 conda install mpi4py
 pip install -e .
-```
 
 
-extra package to install if needed:
-`pip install py-cpuinfo`
+# extra package to install if needed:
+pip install py-cpuinfo
 
 
-create a wandb account and get an api key  
-[https://docs.wandb.ai/quickstart/](https://docs.wandb.ai/quickstart/)  
-[https://wandb.ai/authorize](https://wandb.ai/authorize)
+# create a wandb account and get an api key  
+# https://docs.wandb.ai/quickstart/)  
+# https://wandb.ai/authorize
 
-Then run this code:
-```
+# Then run this code:
 export WANDB_API_KEY=<your_api_key>
 pip install wandb
 wandb login
-```
 
 
-Merge dora weights (I haven't tested this, don't know if its necessary)
-```
+# Merge dora weights (I haven't tested this, don't know if its necessary)
 bash train.sh
 bash ./scripts/run_merge_dora.sh \
  --model_name_or_path Qwen/Qwen1.5-1.8B \
  --lora_model_path output_models/dora \
  --output_model_path output_models/dora_merged \
- ```
 
 
-Evaluation (I removed this line of code: `cache_dir=~/.cache` because it wasn't working):
-```
+# Evaluation (I removed this line of code: `cache_dir=~/.cache` because it wasn't working):
 cd LMFlow/lm-evaluation-harness
 pip install -e . 
 
@@ -48,4 +42,3 @@ lm_eval --model hf \
     --batch_size 1 \
     --log_samples \
     --output_path ./eval_results/test_elmb
-```
