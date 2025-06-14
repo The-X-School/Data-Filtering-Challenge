88fd7e67c539fa97aa8f83bc959ab829c1ef8773
Lindsey Li <lli065@student.fuhsd.org>
2025-06-14 13:33:00 -0700

    format data

--- download_climblab.py
+++ download_climblab.py
@@ -2,17 +2,43 @@
 # Script to download the ClimbLab dataset from Hugging Face
 
 from datasets import load_dataset
+from itertools import islice
 import os
+import json
 
 # Set output directory
-output_dir = "data/climblab_sample"
+output_dir = "data"
 os.makedirs(output_dir, exist_ok=True)
 
 # Load the ClimbLab dataset (default split)
-dataset = load_dataset("nvidia/ClimbLab", split="train", streaming=True)
+# use text dataset instead of tokenized dataset
+dataset = load_dataset("OptimalScale/ClimbLab", split="train", streaming=True)
 
-sample = dataset.select(range(100))
-sample_path = os.path.join(output_dir, "climblab_sample.jsonl")
-sample.to_json(sample_path, orient="records", lines=True)
+# sample = dataset.select(range(100)) doesn't work with streaming dataset
+sample = list(islice(dataset, 100))
+for ex in sample[:3]:
+    print(ex)
+
+'''
+format the data to look like this:  
+{
+  "type": "text_only",
+  "instances": [
+    {  "text": "SAMPLE_TEXT_1" },
+    {  "text": "SAMPLE_TEXT_2" },
+    {  "text": "SAMPLE_TEXT_3" },
+  ]
+}
+'''
+instances = [{"text": item["text"]} for item in sample]
+
+formatted_sample = {
+    "type": "text_only",
+    "instances": instances
+}
+
+sample_path = os.path.join(output_dir, "climblab_sample.json")
+with open(sample_path, "w", encoding="utf-8") as f:
+    json.dump(formatted_sample, f, ensure_ascii=False, indent=2)
 
 print(f"Downloaded and saved 100 samples from ClimbLab to {sample_path}") 
--- model_evaluation_instructions.sh
+++ model_evaluation_instructions.sh
@@ -1,20 +1,18 @@
 # Model evaluation instructions 
-#I changed a few lines of code, it worked when I ran it on command line on Mac
+# I changed a few lines of code, it worked when I ran it on command line on Mac
 
-git clone -b data4elm https://github.com/OptimalScale/LMFlow.git
-cd LMFlow
+git clone https://github.com/The-X-School/Data-Filtering-Challenge.git
+cd Data-Filtering-Challenge
 conda create -n lmflow python=3.9 -y
 conda activate lmflow
 conda install mpi4py
 pip install -e .
 
-
 # extra package to install if needed:
 pip install py-cpuinfo
 
-
 # create a wandb account and get an api key  
-# https://docs.wandb.ai/quickstart/)  
+# https://docs.wandb.ai/quickstart/
 # https://wandb.ai/authorize
 
 # Then run this code:
@@ -22,21 +20,22 @@ export WANDB_API_KEY=<your_api_key>
 pip install wandb
 wandb login
 
-
-# Merge dora weights (I haven't tested this, don't know if its necessary)
+# here's the training script, update it with the filtered dataset before running it:
+# https://github.com/The-X-School/Data-Filtering-Challenge/blob/main/train.sh
 bash train.sh
+
+# Merge dora weights, used when fine-tuning with our dataset
 bash ./scripts/run_merge_dora.sh \
  --model_name_or_path Qwen/Qwen1.5-1.8B \
  --lora_model_path output_models/dora \
  --output_model_path output_models/dora_merged \
 
-
 # Evaluation (I removed this line of code: `cache_dir=~/.cache` because it wasn't working):
-cd LMFlow/lm-evaluation-harness
+cd Data-Filtering-Challenge/lm-evaluation-harness
 pip install -e . 
 
 lm_eval --model hf \
-    --model_args pretrained=[YOUR_MODEL_PATH],trust_remote_code=True \
+    --model_args pretrained=data4elm/Llama-400M-12L,trust_remote_code=True \
     --tasks elmb_roleplay,elmb_reasoning,elmb_functioncalling,elmb_chatrag \
     --device cuda:0 \
     --batch_size 1 \
