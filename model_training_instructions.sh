042097a40baea74496989375b5cb7c1da57fcf11
Lindsey Li <lli065@student.fuhsd.org>
2025-06-16 12:09:54 -0700

    fixed training code

--- model_training_instructions.sh
+++ model_training_instructions.sh
@@ -30,6 +30,6 @@ wandb login
 # Merge dora weights
 bash train.sh
 bash ./scripts/run_merge_dora.sh \
- --model_name_or_path Qwen/Qwen1.5-1.8B \
- --lora_model_path output_models/dora \
+ --model_name_or_path data4elm/Llama-400M-12L \
+ --lora_model_path output_models/finetune \
  --output_model_path output_models/dora_merged \
--- train.sh
+++ train.sh
@@ -57,7 +57,7 @@ deepspeed ${deepspeed_args} \
     --num_train_epochs 1 \
     --learning_rate 1e-4 \
     --block_size 1024 \
-    --per_device_train_batch_size 24 \
+    --per_device_train_batch_size 1 \
     --use_dora 1 \
     --lora_r 16 \
     --lora_target_modules="embed_tokens,q_proj,k_proj,v_proj,o_proj,gate_proj,up_proj,down_proj,lm_head" \
