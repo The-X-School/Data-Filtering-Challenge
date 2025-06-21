ebbc33a6e9dac40c6a1895cb9b16617396c15e18
wes1315 <yuxing.g@gmail.com>
2025-06-21 11:05:22 -0700

    Refactor model setup and evaluation instructions
    - Moved setup instructions from model_evaluation_instructions.sh to a new model_setup_instructions.sh file for better organization.
    - Cleaned up model_evaluation_instructions.sh by removing unnecessary lines and clarifying the evaluation process.
    - Ensured consistency in package installation commands across scripts.

--- model_evaluation_instructions.sh
+++ model_evaluation_instructions.sh
@@ -1,28 +1,6 @@
 # Model evaluation instructions 
 #I changed a few lines of code, it worked when I ran it on command line on Mac
 
-git clone https://github.com/The-X-School/Data-Filtering-Challenge
-cd Data-Filtering-Challenge 
-
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
-bash miniconda.sh -b -p $HOME/miniconda3
-rm miniconda.sh
-eval "$($HOME/miniconda3/bin/conda shell.bash hook)"
-conda init bash
-source ~/.bashrc
-
-#To Check if conda is installed:
-conda --version 
-conda create -n lmflow python=3.9 -y
-conda activate lmflow
-conda install mpi4py
-pip install -e .
-
-
-# extra package to install if needed:
-pip install py-cpuinfo
-pip install -r requirements.txt
-
 # Evaluation (I removed this line of code: `cache_dir=~/.cache` because it wasn't working):
 cd ~/Data-Filtering-Challenge/lm-evaluation-harness
 pip install -e . 
