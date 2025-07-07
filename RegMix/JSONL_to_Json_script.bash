ec1154de902d038b01ccd954b2842ca08f9b78c4
Evan Zhao <evanzhao@Mac.attlocal.net>
2025-07-07 10:58:55 -0700

    update bash file

--- RegMix/JSONL_to_Json_script.bash
+++ RegMix/JSONL_to_Json_script.bash
@@ -1,5 +1,9 @@
+mkdir -p /home/ubuntu/Data-Filtering-Challenge/RegMix/Json_Data/
 for i in {1..20}; do
-    input="cluster_$i.JSONL"
-    output="cluster_$i.json"
-    echo $input
+
+    i_path="/home/ubuntu/Data-Filtering-Challenge/RegMix/Json_Data/cluster_$i.JSONL"
+    o_path="/home/ubuntu/Data-Filtering-Challenge/RegMix/Json_Data/cluster_$i.json"
+
+    python json_to_jsonl $i_path $o_path
+
 done
