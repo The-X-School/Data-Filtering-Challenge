f5b536fd15d0c1b2298494dc57386540888621cd
Evan Zhao <evanzhao@Mac.attlocal.net>
2025-07-07 11:00:20 -0700

    debugs

--- RegMix/JSONL_to_Json_script.bash
+++ RegMix/JSONL_to_Json_script.bash
@@ -4,6 +4,6 @@ for i in {1..20}; do
     i_path="/home/ubuntu/Data-Filtering-Challenge/RegMix/Json_Data/cluster_$i.JSONL"
     o_path="/home/ubuntu/Data-Filtering-Challenge/RegMix/Json_Data/cluster_$i.json"
 
-    python json_to_jsonl $i_path $o_path
+    python json_to_jsonl.py $i_path $o_path
 
 done
