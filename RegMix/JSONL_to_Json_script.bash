c998a2f0f83d858a4d11d972f7014bf37a7ebcf0
Evan Zhao <evanzhao@Mac.attlocal.net>
2025-07-07 11:09:02 -0700

    update to JSONL converter

--- RegMix/JSONL_to_Json_script.bash
+++ RegMix/JSONL_to_Json_script.bash
@@ -1,9 +1,23 @@
+python hf_climblab.py
+
 mkdir -p /home/ubuntu/Data-Filtering-Challenge/RegMix/Json_Data/
+mv json_to_jsonl.py JsonL_Data/
+
+cd JsonL_Data/
+
 for i in {1..20}; do
 
-    i_path="/home/ubuntu/Data-Filtering-Challenge/RegMix/Json_Data/cluster_$i.JSONL"
-    o_path="/home/ubuntu/Data-Filtering-Challenge/RegMix/Json_Data/cluster_$i.json"
+    i_path="cluster_$i.JSONL"
+    o_path="cluster_$i.json"
 
     python json_to_jsonl.py $i_path $o_path
 
+cd ..
+
+for i in {1..20}; do
+
+    filename="/home/ubuntu/Data-Filtering-Challenge/RegMix/JsonL_Data/cluster_$i.json" 
+    mv $filename Json_Data/
+
+rm -rf JsonL_Data/
 done
