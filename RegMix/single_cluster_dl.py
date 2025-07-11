640753647c8a7dad9e1216557153fee3905f1f82
Evan Zhao <evanzhao@Mac.attlocal.net>
2025-07-11 15:18:33 -0700

    new

---
+++ RegMix/single_cluster_dl.py
@@ -0,0 +1,115 @@
+#designed to import from https://huggingface.co/datasets/OptimalScale/ClimbLab/tree/main
+#regmix but im lowk working on inputting now
+#need to install fastparquet and pandas beforehand jsyk
+from huggingface_hub import hf_hub_download
+from huggingface_hub import login
+from huggingface_hub import list_repo_files
+from huggingface_hub import get_hf_file_metadata
+from huggingface_hub import hf_hub_url
+import random
+import os
+import pandas as pd
+import json
+
+HF_TOKEN = "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"
+login(token=HF_TOKEN)
+
+cluster_pos = 2;
+
+#get the list of files in the dataset
+files = list_repo_files(repo_id="OptimalScale/ClimbLab", repo_type="dataset", token=HF_TOKEN)
+print(f"file name: {files}")
+
+
+
+cluster_order = [1,10,11,12,13,14,15,16,17,18,19,2,20,3,4,5,6,7,8,9]
+predicted_dist = [0.587189, 1.000000, 0.082324, 0.577493, 0.574668, 0.580747, 0.085329, 1.000000, 0.574213, 0.578897, 0.600457, 0.587480, 0.576953, 0.581199, 0.590750, 0.580672, 0.575514, 0.585379, 0.585687, 0.579300]
+
+cluster_distribution = [0]*20
+
+total_size = 0
+
+
+
+cluster_distribution[cluster_pos-1] = 10000
+
+print(f"cluster_distribution: {cluster_distribution}")
+
+print("creating cluster distribution files....")
+outfile = "cluster_dist.txt"
+with open(outfile, "w") as f:
+    f.write(str(cluster_distribution))
+print("file created as 'cluster_dist.txt' \n ")
+
+# print(f"\n Total size: {total_size/(1024*1024*1024)} GB")
+
+df_dt_totalsize = 0
+
+#create folder for data
+foldername = "JsonL_Data"
+try:
+    os.mkdir(foldername)
+    print(f"Folder {foldername} created successfully in the current directory.")
+except FileExistsError:
+    print(f"Folder {foldername} already exists.")
+except Exception as e:
+    print(f"An error occurred: {e}")
+
+
+
+for i in range(20):
+    current_pos = cluster_order[i]-1
+    filename = files[2+i*100]
+    print(f"filename {filename}")
+
+    #download the dataset
+    dataset = hf_hub_download(
+        repo_id="OptimalScale/ClimbLab",
+        filename=filename,
+        repo_type="dataset",
+        force_download = True
+    )
+
+    print(f"\nConverting parquet {filename} into dataframe...")
+    dataframe_dataset = pd.read_parquet(dataset, engine = 'fastparquet')
+    #print the dataset
+    print(f"Parquet Conversion of {filename} complete!")
+    print(dataframe_dataset)
+
+    cluster_size = int(cluster_distribution[current_pos])
+    sliced_rows = dataframe_dataset.iloc[0:cluster_size]
+    # print(f"\n\n first 100 rows of df dataset from cluster {1}: {first_100_rows}")
+         # Get file size in bytes
+#    if(cluster_distribution[current_pos] == 0):
+#        sliced_rows = dataframe_dataset.iloc[0]
+
+    json_sliced = sliced_rows.to_json(
+        orient="records",
+        lines=True
+    )
+
+    filename = f"cluster_{current_pos+1}.json"
+
+    if(cluster_size !=0):
+        file_path = os.path.join(foldername, filename)
+
+        with open(file_path, "w") as file:
+            file.write(json_sliced)
+    else:
+        print(f"file 'cluster_{current_pos+1}.json' not created: No Data")
+        
+    
+    df_dt_totalsize += sliced_rows.size
+    #convert to json + turn into another file
+
+    file_size_bytes = os.path.getsize(dataset)
+    print(f'''
+          \nIteration {i}
+          \nCluster {current_pos+1}
+          \nFilename {filename}
+          \nFile size: {sliced_rows.size/1024:.2f}Kb
+          \nTotal File size: {df_dt_totalsize/(1024*1024):.2f} Mb
+          \n First {cluster_size} rows: {sliced_rows} \n
+        ''')
+
+# print(f"Total File Size: {df_dt_totalsize/(1024**3):.2f} GB")
