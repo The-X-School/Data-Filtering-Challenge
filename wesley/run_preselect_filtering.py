2bf89a6ece13a2cb16e4e2d47f7821808a105ed2
Lindsey Li <lli065@student.fuhsd.org>
2025-08-30 11:16:19 -0700

    added detokenization to preselect pipeline

--- wesley/run_preselect_filtering.py
+++ wesley/run_preselect_filtering.py
@@ -41,7 +41,7 @@ def main():
     parser.add_argument("--model_path", default="PreSelect-classifier.bin", help="Path to FastText model")
     parser.add_argument("--threshold", type=float, default=0.5, help="Classifier threshold")
     parser.add_argument("--output_dir", default="Data-Filtering-Challenge/data/default_dir", help="Directory to save final output")
-    parser.add_argument("--steps", type=str, default="1,2,3,4", help="Comma-separated list of steps to run (default is 1,2,3,4)")
+    parser.add_argument("--steps", type=str, default="0,1,2,3,4", help="Comma-separated list of steps to run (default is 1,2,3,4)")
 
     args = parser.parse_args()
     Path(args.output_dir).mkdir(parents=True, exist_ok=True)
@@ -52,19 +52,25 @@ def main():
     cluster_num = get_next_cluster_number(args.output_dir, args.input_path)
     cluster_prefix = f"cluster{cluster_num}"
 
+    detokenized_file = os.path.join(args.output_dir, f"{cluster_prefix}_detokenized.jsonl")
     cluster_output_file = os.path.join(args.output_dir, f"{cluster_prefix}.jsonl")
     sorted_file = os.path.join(args.output_dir, f"{cluster_prefix}_sorted.jsonl")
     formatted_file = os.path.join(args.output_dir, f"{cluster_prefix}_formatted.jsonl")
+
+    # Step 0: Detokenize the data
+    if 0 in steps:
+        run_command(f"python {preselect_script_dir}/detokenize_climblab.py --input_path {args.input_path} --output_path {detokenized_file}")
 
     # Step 1: Run preselect_training.py
     if 1 in steps:
         run_command(
             f"python {preselect_script_dir}/preselect_training.py "
-            f"--input_path={args.input_path} "
+            f"--input_path={detokenized_file} "
             f"--output_path={args.output_dir} "
             f"--model_path={args.model_path} "
             f"--threshold={args.threshold}"
         )
+        os.remove(detokenized_file)
         original_jsonl = find_latest_jsonl_file(args.output_dir)
         os.rename(original_jsonl, cluster_output_file)
         print(f"Renamed preselect output to: {cluster_output_file}")
