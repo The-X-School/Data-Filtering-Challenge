12d680c9803e4b40dc747706d9444c231075354a
Accel <99520131+Pigaju@users.noreply.github.com>
2025-09-03 20:13:03 -0500

    Add files via upload

---
+++ PreSelect_to_RegMix.py
@@ -0,0 +1,106 @@
+#designed to import from https://huggingface.co/datasets/OptimalScale/ClimbLab/tree/main
+#regmix but im lowk working on inputting now
+#need to install fastparquet and pandas beforehand jsyk
+from huggingface_hub import hf_hub_download
+from huggingface_hub import login
+from huggingface_hub import list_repo_files
+from huggingface_hub import get_hf_file_metadata
+from huggingface_hub import hf_hub_url
+import random
+import os
+import pandas as pd
+import json
+
+#get the list of files in the dataset
+files = []
+
+for i in range(1,21):
+   filename = f"cluster{i}_formatted.jsonl"
+   files.append(filename)
+
+print(f"file names: {files}")
+
+predicted_dist = [0.801669, 0.894364, 0.887993, 0.803478, 0.801072, 0.802210, 0.885258, 0.881965, 0.806286, 0.801972, 0.803192, 0.802633, 0.803414, 0.801434, 0.432874, 0.803173, 0.802116, 0.801463, 0.801578, 0.801754]
+print()
+print(f"cluster_distribution: {predicted_dist}")
+
+print("creating cluster distribution files....")
+outfile = "cluster_dist.txt"
+with open(outfile, "w") as f:
+    f.write(str(predicted_dist))
+print("file created as 'cluster_dist.txt' \n ")
+
+
+df_dt_totalsize = 0
+
+#create folder for data
+output_foldername = "new_data"
+try:
+    os.mkdir(output_foldername)
+    print(f"Folder {output_foldername} created successfully in the current directory.")
+except FileExistsError:
+    print(f"Folder {output_foldername} already exists.")
+except Exception as e:
+    print(f"An error occurred: {e}")
+
+current_dir = os.getcwd()
+print(f"current directory: {current_dir}")
+
+for i in range(1,21):
+    foldername = "filtered_clusters_example"
+    filename = files[i-1]
+    print(f"filename {filename}")
+
+    
+    print(f"reading cluster {i} data")
+    #getting json file
+    file_path = os.path.join(foldername, filename)
+    if os.path.exists(file_path):
+        with open(file_path, 'r') as file:
+            data = file.read()
+            print(f"Cluster {i} read \n")
+    else:
+        print(f"File {file_path} does not exist \n")
+        continue
+
+
+    print(f"\nConverting jsonl {filename} into dataframe...")
+    dataframe_dataset = pd.read_json(file_path, lines=True)
+    #print the dataset
+    print(f"JsonL Conversion of {filename} complete \n")
+
+    cluster_size = int(predicted_dist[i-1]*100)
+    sliced_rows = dataframe_dataset.iloc[0:cluster_size]
+
+
+
+
+    json_sliced = sliced_rows.to_json(
+        orient="records",
+        lines=True
+    )
+
+    filename = f"cluster_{i}.json"
+
+    if(cluster_size !=0):
+        file_path = os.path.join(output_foldername, filename)
+
+        with open(file_path, "w") as file:
+            file.write(json_sliced)
+    else:
+        print(f"file 'cluster_{i+1}.json' not created: No Data")
+        
+    
+    df_dt_totalsize += sliced_rows.size
+    #convert to json + turn into another file
+
+    print(f'''
+          \nIteration {i}
+          \nCluster {i}
+          \nFilename {filename}
+          \nFile size: {sliced_rows.size/1024:.2f}Kb
+          \nTotal File size: {df_dt_totalsize/(1024*1024):.2f} Mb
+          \n First {cluster_size} rows: {sliced_rows} \n
+        ''')
+
+# print(f"Total File Size: {df_dt_totalsize/(1024**3):.2f} GB")
